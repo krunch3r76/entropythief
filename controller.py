49d5738fad0e32bf6083d5a7c4256cc58e1c03e5
krunch3r76 <biz@u26a4.com>
2021-07-25 19:36:50 -0700

    apply minor changes while debugging

--- controller.py
+++ controller.py
@@ -23,9 +23,8 @@ import utils
 IMAGE_HASH = "238e362f7b52aa21c3f2a26ade9ba3952ae8c715c9efe37af0ef8258"
 MAXWORKERS = 10
 _MEBIBYTE = 2**20
-_MAXPOOLSIZE = _MEBIBYTE # this is the theoretical max
+_MAXPOOLSIZE = 100 * _MEBIBYTE # this is the theoretical max
 MINPOOLSIZE = _MAXPOOLSIZE - 4096 # leave one page room to prevent blocking
-MINPOOLSIZE = 50*_MEBIBYTE
 #MINPOOLSIZE= 30000 - 4096
 BUDGET = 10.0
 kIPC_FIFO_FP = "/tmp/pilferedbits"
