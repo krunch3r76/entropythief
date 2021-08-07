317afcfeda9c230f4343251a068fda161678fde9
krunch3r76 <biz@u26a4.com>
2021-08-07 00:42:02 -0700

    fix devrand argument to worker executable, correct type casts in worker executable to fill buffer completely

--- randwrite.c
+++ randwrite.c
@@ -13,27 +13,27 @@
 
 
 
-char const * kFilename = "/golem/output/result.bin";
+char const * kFilename = "/tmp/w2/result.bin";
 
 
 
-int from_devrand(uint64_t *buf_holding_random_bytes, uint64_t count)
+int from_devrand(uint64_t *buf_holding_random_bytes, uint64_t count_bytes)
 {
 	int STATUS=0;
 	char getrandomBuf[256]; // getrandom guarantees up to 256 bytes per read
 	ssize_t bytes_acquired;
 	size_t next_len_requested;
-	while(count !=0) {
-			if(count >=256) {
+	while(count_bytes !=0) {
+			if(count_bytes >=256) {
 				next_len_requested=256;
 			} else {
-				next_len_requested=count;
+				next_len_requested=count_bytes;
 			}
 			bytes_acquired = getrandom(getrandomBuf, next_len_requested, GRND_RANDOM);
 			memcpy((void *)buf_holding_random_bytes, (void *)getrandomBuf, bytes_acquired);
-			buf_holding_random_bytes+=bytes_acquired;
+			*(char *)&buf_holding_random_bytes += bytes_acquired;
 
-			count-=bytes_acquired;
+			count_bytes-=bytes_acquired;
 	}
 	return STATUS;
 }
@@ -59,15 +59,17 @@ int rdrand_step (uint64_t *rand)
 
 
 
-
-int from_rdrand(uint64_t *buf_holding_random_bytes, uint64_t count)
+// pre: count_bytes is measured by 8
+int from_rdrand(uint64_t *buf_holding_random_bytes, uint64_t count_bytes)
 {
 	int STATUS = 0;
 	uint64_t next_random_value;
-	for (uint64_t i=0; i < count; ++i)
+	uint64_t count_of_eights = count_bytes / 8; 
+	for (uint64_t i=0; i < count_of_eights; ++i)
 	{
 		while (rdrand_step(&next_random_value) != 1) {};
 		*buf_holding_random_bytes++=next_random_value;
+		printf("%llu\n", next_random_value);
 	}
 	return STATUS;
 	
@@ -76,30 +78,32 @@ int from_rdrand(uint64_t *buf_holding_random_bytes, uint64_t count)
 
 
 
-
+// input the count of random bytes requested followed by the algo
 int main(int argc, char *argv[])
 {
 	int STATUS=0;
-	// parse argument 1 for count
+	// parse argument 1 for count_bytes
 	char *end;
 	FILE *fp_to_output_file;
 
-	uint64_t count = strtoull(argv[1], &end, 10);
+	uint64_t count_bytes = strtoull(argv[1], &end, 10);
 	if (*end != '\0') {
 		STATUS = EXIT_FAILURE;
 	}
-	count /= 8; // discard any partial measurement of 8, ie remainder
 
-
-	uint64_t *buf_holding_random_bytes = (uint64_t*)malloc(count*sizeof(*buf_holding_random_bytes));
+	// for now, remove any measure less than 8
+	uint64_t remainder_less_than_eight = count_bytes % 8;
+	count_bytes-=remainder_less_than_eight;
+	uint64_t *buf_holding_random_bytes = (uint64_t*)calloc(count_bytes, 1);
 	uint64_t *beg = buf_holding_random_bytes;
 
 	// determine entropy source
 	if (strncmp(argv[2], "rdrand", 6) == 0) {
-		STATUS = from_rdrand(buf_holding_random_bytes, count);
+		STATUS = from_rdrand(buf_holding_random_bytes, count_bytes);
 		if (STATUS) fprintf(stderr, "Error calling routine to read from cpu.");
 	} else if (strncmp(argv[2], "devrand", 7) == 0) {
-		STATUS = from_devrand(buf_holding_random_bytes, count);
+		fprintf(stderr, "HERE GOES\n");
+		STATUS = from_devrand(buf_holding_random_bytes, count_bytes);
 		if (STATUS) fprintf(stderr, "Error calling routine to read from kernel.");
 	}
 
@@ -111,7 +115,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (STATUS == 0) {
-			if (!fwrite(beg, sizeof(uint64_t), count, fp_to_output_file)) {
+			if (!fwrite(beg, 1, count_bytes, fp_to_output_file)) {
 				STATUS=EXIT_FAILURE;
 				fprintf(stderr, "Error writing to %s.", kFilename);
 			}
