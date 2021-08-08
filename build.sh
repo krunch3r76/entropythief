6b9cbfda9737be01229c5faebe7baee1bac01b6d
krunch3r76 <biz@u26a4.com>
2021-08-07 18:11:40 -0700

    remove extraneous dockerfile

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# build.sh
-# author: krunch3r (KJM github.com/krunch3r76)
-# license: General Poetic License (GPL3)
-
-/usr/bin/cc -I/usr/local/include/python3.9 -fPIC -O0 -std=gnu11 -o rdrand.c.o -c rdrand.c
-/usr/bin/cc -shared -Wl,-soname,librdrand.so -o rdrand.so rdrand.c.o -lpython3.9
-
