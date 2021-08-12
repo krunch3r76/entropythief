cbb072c5f50820690cb4064ae079a05aff752541
krunch3r76 <biz@u26a4.com>
2021-08-12 14:27:32 -0700

    add a burn nonce count feature to print_nonce

--- readers/print_nonce/print_nonce.py
+++ readers/print_nonce/print_nonce.py
@@ -9,12 +9,21 @@ PATH_TO_PIPE_MODULE=os.path.dirname(__file__) + "/.."
 sys.path.append(PATH_TO_PIPE_MODULE)
 import pipe_reader
 
-readerPipe = pipe_reader.PipeReader()
 
-while True:
-    count = int(input("How many nonces? "))
-    for _ in range(count):
-        result = readerPipe.read(8)
-        int64 = int.from_bytes(result, byteorder="little")
-        print(int64)
+readerPipe = pipe_reader.PipeReader()
+if len(sys.argv) == 1:
+    while True:
+        count = int(input("How many nonces? "))
+        for _ in range(count):
+            result = readerPipe.read(8)
+            int64 = int.from_bytes(result, byteorder="little")
+            print(int64)
+elif len(sys.argv) == 3:
+        if sys.argv[1] == 'burn':
+            count = int(sys.argv[2])
+            print(f"burning {8*count} bytes")
+            readerPipe.read(8*count)
+        else:
+            print(f"usage: {sys.argv[0]} burn <nonce count [counts of 8 bytes]>")
+            sys.exit(127)
 
