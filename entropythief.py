1f63fef71a01530c8104550d5c06992d65f8dc33
krunch3r76 <biz@u26a4.com>
2022-05-25 09:23:14 -0700

    add more version checks including Python3.10 warning

--- entropythief.py
+++ entropythief.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 import sys
 
+
 try:
     import yapapi
 
     version_list = yapapi.__version__.split(".")
     if int(version_list[1]) < 9:
         print(
-            f"Sorry, the version of a required python module, yapapi, is outdated. Currently it is {yapapi.__version__}. It needs to be updated to at least version 0.9.1 to run."
+            f"Sorry, the version of a required python module, yapapi, is outdated."
+            f" Currently it is {yapapi.__version__}. It needs to be updated to at least version 0.9.1 to run."
         )
         sys.exit(1)
 except ModuleNotFoundError:
     print(
-        "Uh oh, did you forget to install the requirements? I could not find a required module: yapapi! Please see the readme for direction."
+        "Uh oh, did you forget to install the requirements? I could not find a required module:"
+        " yapapi! Please see the readme for direction."
     )
     sys.exit(1)
 
@@ -25,15 +28,23 @@ from entropythief.utils import (
     TEXT_COLOR_DEFAULT,
 )
 
+
 parser = build_parser("pilfer entropy stashes from providers")
 args = parser.parse_args()
 print_env_info(args)
+print()
 try:
-    input(
-        "to change any of the settings, ctrl-c and pass the arguments desired on the command line.\n"
-        f"for example, \033[4m./entropythief.py --payment-network {TEXT_COLOR_YELLOW}polygon{TEXT_COLOR_DEFAULT}\033[4m --subnet-tag {TEXT_COLOR_YELLOW}public-beta{TEXT_COLOR_DEFAULT}.\n"
-        "press enter to accept the above arguments and proceed"
+    print(
+        "to change any of the settings, ctrl-c and pass the arguments desired on the command line."
+        f" for example:\n\033[4m./entropythief.py --payment-network {TEXT_COLOR_YELLOW}polygon{TEXT_COLOR_DEFAULT}\033[4m --subnet-tag {TEXT_COLOR_YELLOW}public-beta{TEXT_COLOR_DEFAULT}.\n"
     )
+    if sys.version_info >= (3, 10):
+        print(
+            f"\033[1mWARNING\033[0m You are using Python version {sys.version}, but"
+            " only earlier versions have been tested. Please consider using Python"
+            " version 3.9.x or less instead. \033[1mWARNING\033[0m"
+        )
+    input("press enter to accept the above arguments and proceed")
     print("")
 except KeyboardInterrupt:
     print("\n\033[1msure thing boss, see you in a few\033[0m")
